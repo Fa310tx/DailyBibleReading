7909564d832c339e4dee3058d626550701c4d49b
Joshua Jackson <jj@anti-exe.com>
2017-06-14 22:14:57 -0500

    Initital setup, API classes, and link UWP to Store

---
+++ DailyBibleReading/DailyBibleReading/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DailyBibleReading
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new DailyBibleReading.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+			// Handle when your app starts
+			// azure mobile analytics
+			MobileCenter.Start("android=7d921a5a-c740-47fb-97da-061e018aa5ac;" +
+				   "ios=c8821ea6-ed12-4445-8cc5-b60623bbebce;" +
+				   "uwp=33410f43-a0fc-4185-b696-dda752e6b2da;",				   
+				   typeof(Analytics), typeof(Crashes));
+		}
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ DailyBibleReading/DailyBibleReading/Helper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DailyBibleReading
+{
+	public class Helper
+    {
+        // connect to a json api and return the results
+        public static async Task<string> GetJsonAsync(string _url)
+        {
+            // declare an empty variable to be filled later
+            string result = null;
+            HttpClient httpclient = new HttpClient();
+            Uri url = new Uri(_url);
+
+            // Always catch network exceptions for async methods
+            try
+            {
+                result = await httpclient.GetStringAsync(url);
+
+                // do something with the results
+                // Output to debugger
+                //Debug.WriteLine(result);
+                // create a popup with the results
+                //MessageDialog dialog = new MessageDialog(result);
+                //await dialog.ShowAsync();
+            }
+            catch (Exception ex)
+            {
+                // Details in ex.Message and ex.HResult.
+                // do something with the results
+                // Output to debugger
+                Debug.WriteLine(ex);
+                // create a popup with the results
+                //MessageDialog dialog = new MessageDialog(result);
+                //await dialog.ShowAsync();
+            }
+
+            // Once your app is done using the HttpClient object call dispose to free up system resources (the underlying socket and memory used for the object)
+            httpclient.Dispose();
+
+            // return the string to Task<>
+            return result;
+        }
+    }
+}
